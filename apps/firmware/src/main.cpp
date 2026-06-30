13570d63ea1582ca0c3b539c58d4c92192df793c
KevinSotoMiranda <levinsoto5@gmail.com>
2026-06-29 18:29:55 -0600

    Add button struct and refactor code

--- apps/firmware/src/main.cpp
+++ apps/firmware/src/main.cpp
@@ -1,45 +1,53 @@
 #include <Arduino.h>
 
+struct Button {
+  uint8_t pin;
+  bool lastState;
+  bool currentState;
+};
+
 // GPIO pins connected to button
-const uint8_t button_pins[] = {
-  18,
-  19,
-  17,
-  16,
-  21,
-  22
+Button buttons[] = {
+  {18},
+  {19},
+  {17},
+  {16},
+  {21},
+  {22}
 }; 
 
-const int num_buttons = 6;
+const int NUM_BUTTONS = sizeof(buttons) / sizeof(buttons[0]);
 
-int lastState[num_buttons]; // the previous i state from the input pin
-int currentState[num_buttons]; // the current i reading from the input pin
+// int lastState[num_buttons]; // the previous i state from the input pin
+// int currentState[num_buttons]; // the current i reading from the input pin
 
 // put your setup code here, to run once:
 void setup() {
   Serial.begin(115200);
 
   // initialize the pushbuttons pin as an pull-up input
-  for(int i = 0; i < num_buttons; i++) {
-    pinMode(button_pins[i], INPUT_PULLUP);
-    lastState[i] = digitalRead(button_pins[i]);
+  for(int i = 0; i < NUM_BUTTONS; i++) {
+    pinMode(buttons[i].pin, INPUT_PULLUP);
+
+    buttons[i].currentState = digitalRead(buttons[i].pin);
+    buttons[i].lastState = buttons[i].currentState;
   }
 }
 
 // put your main code here, to run repeatedly:
 void loop() {
   // read the state of the switch/button:
-  for(int i = 0; i < num_buttons; i++) {
-    currentState[i] = digitalRead(button_pins[i]);
+  for(int i = 0; i < NUM_BUTTONS; i++) {
+    buttons[i].currentState = digitalRead(buttons[i].pin);
   }
 
-  for(int i = 0; i < num_buttons; i++) {
-    if(lastState[i] == LOW && currentState[i] == HIGH)
+  for(int i = 0; i < NUM_BUTTONS; i++) {
+    if(buttons[i].lastState == LOW && buttons[i].currentState == HIGH)
       Serial.printf("Button %d was pressed\n", i + 1);
   }  
 
   // save the last state
-  for(int i = 0; i < num_buttons; i++) {
-    lastState[i] = currentState[i];
+  for(int i = 0; i < NUM_BUTTONS; i++) {
+    buttons[i].lastState = buttons[i].currentState;
   } 
 }
